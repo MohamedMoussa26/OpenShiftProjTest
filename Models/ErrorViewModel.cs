1e72c05f0e5f4a8884b83dbd3abcf5d42082d777
Mohamed Moussa <Mohamed.Moussa@linkdev.com>
2022-07-27 19:46:10 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OpenShiftProj.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
